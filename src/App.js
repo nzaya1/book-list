b0c2b088e2ea2e4ea57a932651d33acd4fe9b762
nzaya1 <86688312+nzaya1@users.noreply.github.com>
2021-07-15 01:16:43 -0400

    vs code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+import book from './components/book';
+import booklist from './components/booklist';
+
+class App extends Component {
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="containter card mt-4 p-4">
+     
+     <div className="text-center">
+       <h1> BookList</h1>
+     </div>
+      
+      <hr/>
+      <div className="text-center">
+        <h2> Add Book:</h2>
+      </div>
+ 
+      <div className="mb-3">
+  <label for="formGroupExampleInput" className="form-label">Author</label>
+  <input type="text" className="form-control" id="formGroupExampleInput" placeholder=" "/>
+</div>
+<div className="mb-3">
+  <label for="formGroupExampleInput2" className="form-label">Title</label>
+  <input type="text" className="form-control" id="formGroupExampleInput2" placeholder=""/>
+</div>
+<div className="mb-3">
+  <label for="formGroupExampleInput2" className="form-label">ISBN#</label>
+  <input type="text" className="form-control" id="formGroupExampleInput2" placeholder=""/>
+</div>
+
+  
+<book/>
+<booklist/>
+     
+
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/components/booklist.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class booklist extends Component {
+    render() {
+        return (
+            <div>
+                booklist
+            </div>
+        )
+    }
+}
